f1fe49b434337458594716f916ef6d515bba7376
Brytanni <99222703+BrytanniADJ@users.noreply.github.com>
2024-05-09 16:57:45 -0300

    UP

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def chamar_principal():
 # pag-inicial.ui
 def inicial_estoque(): # cadastros
     principal.close()
+    cadastros.close()
     estoque.show()
 
 def cadastros_inicial(): # cadastro de produtos
@@ -57,7 +58,7 @@ def voltar():
     cadastros.close()
     principal.show()
 
-def banco_cad_fun():
+def buscarProd():
     import mysql.connector
 
     # Estabelecer a conexão com o banco de dados
@@ -70,7 +71,100 @@ def banco_cad_fun():
     )
     cursor = banco.cursor()
 
-    # Obter os dados do formulário
+    colunas = f"nome_produto, tamanho_produto, modelo_produto, cor_produto, marca_produto, preco_custo, preco_venda, qtd_produto"
+    prodCod = cadastros.prodCod.text()
+
+    try:
+        query = f"SELECT {colunas} FROM produtos WHERE cod_produto = {prodCod}"
+        cursor.execute(query)
+
+        selecao = cursor.fetchall()
+
+        prodNome = cadastros.prodNome.setText(str(selecao[0][0]))
+        prodTamanho = cadastros.boxTamanho.setText(str(selecao[0][1]))
+        prodModelo = cadastros.prodModelo.setText(str(selecao[0][2]))
+        prodCor = cadastros.prodCor.setText(str(selecao[0][3]))
+        prodMarca = cadastros.prodMarca.setText(str(selecao[0][4]))
+        prodPrecoCusto = cadastros.prodPrecoCusto.setText(str(selecao[0][5]))
+        prodPrecoVenda = cadastros.prodPrecoVenda.setText(str(selecao[0][6]))
+        prodQtd = cadastros.prodQuant.setText(str(selecao[0][7]))
+    except:
+        erro_produto = QtWidgets.QErrorMessage()
+        erro_produto.showMessage('Código não existente')
+        erro_produto.exec_()
+def buscarFun():
+    import mysql.connector
+
+    # Estabelecer a conexão com o banco de dados
+    banco = mysql.connector.connect(
+        host='localhost',
+        port='3306',
+        user='root',
+        password='123456',
+        database='meteoro_calcados'
+    )
+    cursor = banco.cursor()
+
+    if cadastros.radioCliente.isChecked():
+        tabela = 'cliente'
+    else:
+        tabela = 'funcionario'
+
+    colunas = f"nome_{tabela}, sobrenome_{tabela}, cpf_{tabela}, telefone_{tabela}, email_{tabela}, endereco_{tabela}"
+    funCod = cadastros.funCod.text()
+
+    try:
+        query = f"SELECT {colunas} FROM {tabela} WHERE cod_{tabela} = {funCod}"
+        cursor.execute(query)
+
+        selecao = cursor.fetchall()
+
+        funNome = cadastros.funNome.setText(str(selecao[0][0]))
+        funSobre = cadastros.funSobre.setText(str(selecao[0][1]))
+        funCPF = cadastros.funCPF.setText(str(selecao[0][2]))
+        funTell = cadastros.funTell.setText(str(selecao[0][3]))
+        funEmail = cadastros.funEmail.setText(str(selecao[0][4]))
+        endCod = cadastros.endCod.setText(str(selecao[0][5]))
+
+        buscarFun2()
+        
+    except:
+        erro_login = QtWidgets.QErrorMessage()
+        erro_login.showMessage('Código não existente')
+        erro_login.exec_()
+
+def buscarFun2():
+    cursor = banco.cursor()
+    endCod = cadastros.endCod.text()
+
+    colunas = f"cep, estado, cidade, bairro, rua, numero_casa"
+    query = f"SELECT {colunas} FROM endereco WHERE cod_endereco = {endCod}"
+    cursor.execute(query)
+    selecao = cursor.fetchall()
+    cursor.close()
+
+    endCep = cadastros.funCep.setText(str(selecao[0][0]))
+    endEstado = cadastros.funEstado.setText(str(selecao[0][1]))
+    endCidade = cadastros.funCidade.setText(str(selecao[0][2]))
+    endBairro = cadastros.funBairro.setText(str(selecao[0][3]))
+    endRua = cadastros.funRua.setText(str(selecao[0][4]))
+    endNumero = cadastros.funNmr.setText(str(selecao[0][5]))
+
+#bancos
+def banco_cad_fun():
+    import mysql.connector
+    global botao
+
+    # Estabelecer a conexão com o banco de dados
+    banco = mysql.connector.connect(
+        host='localhost',
+        port='3306',
+        user='root',
+        password='123456',
+        database='meteoro_calcados'
+    )
+    cursor = banco.cursor()
+
     funCod = cadastros.funCod.text() # não irá se usar na query
     funNome = cadastros.funNome.text()
     funSobre = cadastros.funSobre.text()
@@ -78,6 +172,7 @@ def banco_cad_fun():
     funTell = cadastros.funTell.text()
     funEmail = cadastros.funEmail.text()
 
+    endCod = cadastros.endCod.text()
     endCep = cadastros.funRua.text()
     endEstado = cadastros.funEstado.text()
     endCidade = cadastros.funCidade.text()
@@ -85,23 +180,61 @@ def banco_cad_fun():
     endRua = cadastros.funRua.text()
     endNumero = cadastros.funNmr.text()
 
-    # Determinar a tabela e as colunas
     if cadastros.radioCliente.isChecked():
         tabela = 'cliente'
     else:
         tabela = 'funcionario'
 
-    colunas = f"nome_{tabela}, sobrenome_{tabela}, cpf_{tabela}, telefone_{tabela}, email_{tabela}"
+    colunas = f"nome_{tabela}, sobrenome_{tabela}, cpf_{tabela}, telefone_{tabela}, email_{tabela}, endereco_{tabela}"
 
-    # Query SQL para inserir dados do formulário
-    query = f"INSERT INTO {tabela} ({colunas}) VALUES (%s, %s, %s, %s, %s)"
-    values = (funNome, funSobre, funCPF, funTell, funEmail)
-    cursor.execute(query, values)
+    if botao == 'salvar':
+        try:
+            # Query SQL para inserir dados do formulário
+            query = f"INSERT INTO {tabela} ({colunas}) VALUES (%s, %s, %s, %s, %s)"
+            values = (funNome, funSobre, funCPF, funTell, funEmail, endCod)
+            cursor.execute(query, values)
 
-    # Query SQL para inserir endereço
-    query2 = "INSERT INTO endereco (cep, estado, cidade, bairro, rua, numero_casa) VALUES (%s, %s, %s, %s, %s, %s)"
-    values2 = (endCep, endEstado, endCidade, endBairro, endRua, endNumero)
-    cursor.execute(query2, values2)
+            # Query SQL para inserir endereço
+            query2 = "INSERT INTO endereco (cod_endereco, cep, estado, cidade, bairro, rua, numero_casa) VALUES (%s, %s, %s, %s, %s, %s)"
+            values2 = (endCod, endCep, endEstado, endCidade, endBairro, endRua, endNumero)
+            cursor.execute(query2, values2)
+        except:
+            erro_salvar= QtWidgets.QErrorMessage()
+            erro_salvar.showMessage('Código não existente')
+            erro_salvar.exec_()
+            print("Código não existente [salvar]")
+
+    elif botao == 'excluir':
+        try:
+            # Query SQL para inserir dados do formulário
+            query = f"DELETE FROM {tabela} WHERE cod_{tabela} = {funCod}"
+            cursor.execute(query)
+
+            # Query SQL para inserir endereço
+            query = f"DELETE FROM endereco WHERE cod_endereco = {endCod}"
+            cursor.execute(query)
+        except:
+            erro_salvar= QtWidgets.QErrorMessage()
+            erro_salvar.showMessage('Código não existente')
+            erro_salvar.exec_()
+            print("Código não existente [excluir]")
+
+    elif botao == 'update':
+        try:
+            # Query SQL para inserir dados do formulário
+            query = f"UPDATE {tabela} SET nome_{tabela} = {funNome}, sobrenome_{tabela} = {funSobre}, cpf_{tabela} = {funCPF}, telefone_{tabela} = {funTell}, email_{tabela} = {funEmail}, endereco_{tabela} = {endCod} WHERE cod_{tabela} = {funCod}"
+            cursor.execute(query)
+
+            # Query SQL para inserir endereço
+            query = f"UPDATE endereco SET cod_endereco = {endCod}, cep = {endCep}, estado = {endEstado}, cidade = {endEstado}, bairro = {endBairro}, rua = {endRua}, numero_casa = {endNumero}"
+            cursor.execute(query)
+        except:
+            erro_salvar= QtWidgets.QErrorMessage()
+            erro_salvar.showMessage('Código não existente')
+            erro_salvar.exec_()
+            print("Código não existente [update]")
+    else:
+        print("Erro nas condições")
 
     # Commit e fechamento de conexão
     banco.commit()
@@ -115,7 +248,8 @@ def banco_cad_fun():
     funTell = cadastros.funTell.setText('')
     funEmail = cadastros.funEmail.setText('')
 
-    endCep = cadastros.funRua.setText('')
+    endCod = cadastros.endCod.setText('')
+    endCep = cadastros.funCep.setText('')
     endEstado = cadastros.funEstado.setText('')
     endCidade = cadastros.funCidade.setText('')
     endBairro = cadastros.funBairro.setText('')
@@ -163,6 +297,7 @@ def banco_cad_prod():
     prodPrecoCusto = cadastros.prodPrecoCusto.setText('')
     prodPrecoVenda = cadastros.prodPrecoVenda.setText('')
     prodQuant = cadastros.prodQuant.setText('')
+
 # definição de funções (campos)
 def campos_login():
     global usuario
@@ -188,6 +323,36 @@ def campos_login():
     cursor.close()
     banco.close()
 
+# atualização, exclusão, salvamento...
+def salvar_prod():
+    global botao
+    botao = 'salvar'
+    banco_cad_prod()
+
+def excluir_prod():
+    global botao
+    botao = 'excluir'
+    banco_cad_prod()
+
+def update_prod():
+    global botao
+    botao = 'update'
+    banco_cad_prod()
+
+def salvar_fun():
+    global botao
+    botao = 'salvar'
+    banco_cad_fun()
+
+def excluir_fun():
+    global botao
+    botao = 'excluir'
+    banco_cad_fun()
+
+def update_fun():
+    global botao
+    botao = 'update'
+    banco_cad_fun()
 
 # pesquisas
 def botao_pesquisa_cod():
@@ -259,6 +424,10 @@ def banco_pesquisa_prod():
         for i in range(len(selecao)):
             for j in range(7):
                 estoque.dataTree.setItem(i, j, QtWidgets.QTableWidgetItem(str(selecao[i][j])))
+    except:
+        erro_pesquisa = QtWidgets.QErrorMessage()
+        erro_pesquisa.showMessage('Pesquisa não encontrada')
+        erro_pesquisa.exec_()
 
 def pesquisa_geral_prod():
     import mysql.connector
@@ -301,13 +470,24 @@ login.bt_exit.clicked.connect(exit)
 principal.bt_cad.clicked.connect(cadastros_inicial) # botão 'cadastro de produtos' --> tela 'cadastro'
 principal.bt_estoque.clicked.connect(inicial_estoque) # botão 'estoque' --> tela 'estoque']
 principal.bt_disconnect.clicked.connect(disconect)
+cadastros.bt_consultaProd.clicked.connect(inicial_estoque)
+cadastros.bt_consultaFun.clicked.connect(inicial_estoque)
 # botão voltar
 cadastros.bt_voltar.clicked.connect(voltar)
 estoque.bt_voltar.clicked.connect(voltar)
 
 # cadastros
-cadastros.bt_salvarFun.clicked.connect(banco_cad_fun)
-cadastros.bt_salvarProd.clicked.connect(banco_cad_prod)
+cadastros.bt_salvarFun.clicked.connect(salvar_fun)
+cadastros.bt_salvarProd.clicked.connect(salvar_prod)
+
+cadastros.bt_excluirProd.clicked.connect(excluir_prod)
+cadastros.bt_excluirFun.clicked.connect(excluir_fun)
+
+cadastros.bt_updateFun.clicked.connect(update_fun)
+cadastros.bt_updateProd.clicked.connect(update_prod)
+
+cadastros.bt_buscarFun.clicked.connect(buscarFun)
+cadastros.bt_buscarProd.clicked.connect(buscarProd)
 
 # select table
 estoque.bt_cod.clicked.connect(botao_pesquisa_cod)
